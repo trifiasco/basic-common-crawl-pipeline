5fbfa3bd695e2e4764bb196306a22487da674254
Arnab Roy <trifiasco@gmail.com>
2025-10-18 19:20:05 +0200

    feat: add further progress monitoring in worker

--- python/src/commoncrawl_pipeline/worker.py
+++ python/src/commoncrawl_pipeline/worker.py
@@ -15,6 +15,15 @@ from commoncrawl_pipeline.config import (
 from commoncrawl_pipeline.rabbitmq import rabbitmq_channel
 
 batch_counter = Counter("worker_batches", "Number of consumed batches")
+batch_items_total = Counter(
+    "worker_batch_items_total", "Total items received in batches"
+)
+batch_items_processed = Counter(
+    "worker_batch_items_processed", "Batch items successfully processed"
+)
+download_bytes_total = Counter(
+    "worker_download_bytes_total", "Total bytes downloaded from Common Crawl"
+)
 warc_records_total = Counter(
     "worker_warc_records_total", "Total WARC records processed"
 )
@@ -36,12 +45,16 @@ documents_extraction_failed = Counter(
 def process_batch(downloader: Downloader, ch, method, _properties, body):
     print("Received batch of size", len(body))
     batch = json.loads(body)
+    batch_items_total.inc(len(batch))
+
     for item in batch:
         data = downloader.download_and_unzip(
             item["metadata"]["filename"],
             int(item["metadata"]["offset"]),
             int(item["metadata"]["length"]),
         )
+        download_bytes_total.inc(len(data))
+
         for record in WARCIterator(io.BytesIO(data)):
             warc_records_total.inc()
             if record.rec_type == "response":
@@ -54,6 +67,9 @@ def process_batch(downloader: Downloader, ch, method, _properties, body):
                     documents_extraction_failed.inc()
             else:
                 warc_records_non_response.inc()
+
+        batch_items_processed.inc()
+
     batch_counter.inc()
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
